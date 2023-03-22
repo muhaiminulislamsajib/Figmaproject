c63827918a0b15aa9a902a967583989f1d237044
Muhaiminul Islam Sajib <muhaiminulislamsajib101@gmail.com>
2023-03-22 14:57:12 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+    $('.slider_main').slick({
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+  });
+});
